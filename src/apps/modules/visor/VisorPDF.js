f42bdd73ca3f55315db3e2e60588412808448046
renzoxpixely <renzo_or@hotmail.com>
2024-04-06 07:25:56 -0500

    fix:visor pdf 100%

--- src/apps/modules/visor/VisorPDF.js
+++ src/apps/modules/visor/VisorPDF.js
@@ -1,4 +1,4 @@
-import React from 'react'  
+import React from 'react';  
 import { pdfjs } from 'react-pdf'; 
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
@@ -6,33 +6,27 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'; 
 import { useParams } from 'react-router-dom';
 
-import {URL_BUCKET} from '../../BackConfig';
-
+import { URL_BUCKET } from '../../BackConfig';
 
 const VisorPDF = () => { 
- 
     const { id } = useParams();
- 
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
       
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-    const pdfFile = `${URL_BUCKET}${id}`
-
+    const pdfFile = `${URL_BUCKET}${id}`;
      
-  return ( 
- 
-    <div style={{ height: '750px' }} >
-      <Viewer 
-          fileUrl={pdfFile}
-          plugins={[defaultLayoutPluginInstance]}
-          defaultScale={1}
-          defaultScaleDelta={1.1}
-          defaultScaleValue="page-width"
-      />
-    </div>
-
-  )
+    return ( 
+        <div style={{ height: '100vh', width: '100vw' }} >
+            <Viewer 
+                fileUrl={pdfFile}
+                plugins={[defaultLayoutPluginInstance]}
+                defaultScale={1}
+                defaultScaleDelta={1.1}
+                defaultScaleValue="page-width"
+            />
+        </div>
+    );
 }
 
-export {VisorPDF}
+export { VisorPDF };
