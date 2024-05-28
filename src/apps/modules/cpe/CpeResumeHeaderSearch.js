325640a7f9dd456c0064bde6079be712f680edaa
renzo <desarrollo.jadalsoftware@gmail.com>
2024-05-28 11:46:13 -0500

    fix:resumen locales

--- src/apps/modules/cpe/CpeResumeHeaderSearch.js
+++ src/apps/modules/cpe/CpeResumeHeaderSearch.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect, useContext} from 'react';
 import { useForm } from "react-hook-form"; 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"; 
+import {getTipoCPE, getEstados, getSedes} from './busqueda/services/CpeService';  
 
 import {useSearch} from './busqueda/core/searchContext';
 import {useAuth} from '../auth';
@@ -16,24 +17,35 @@ const CpeResumeHeaderSearch = () => {
 
   const [startDate, setStartDate] = useState(new Date(moment().startOf('month')));
   const [endDate, setEndDate] = useState(new Date(moment().endOf('month')));
-  
+  const [sede, setSede] = useState([]); 
+
+  const getDataList = async() => { ;
+    let sede = await getSedes({rucEmisor: currentEmisor.rucEmisor}); 
+    setSede(sede); 
+  }
 
   const getDefault = () => {
     let rucEmisor = currentEmisor.rucEmisor;
     let fechaDesde = moment(startDate).format("YYYY-MM-DD");
     let fechaHasta = moment(endDate).format("YYYY-MM-DD");
- 
-    let datos = {...searchCpe,fechaDesde, fechaHasta, rucEmisor}  
- 
+    let Sucursal = (searchCpe.Sucursal === '' || searchCpe.Sucursal === '-' || searchCpe.Sucursal === false) ? '-' : searchCpe.Sucursal;
+    let datos = {...searchCpe,fechaDesde, fechaHasta, rucEmisor, Sucursal}  
     setDataCpe(datos);  
   }
 
-  const manejarSubmit = async () => {   
-    getDefault();
+  const manejarSubmit = async (data) => {   
+    setDataCpe({
+      ...searchCpe,
+      ...data,
+      fechaDesde: moment(startDate).format("YYYY-MM-DD"),
+      fechaHasta: moment(endDate).format("YYYY-MM-DD"),
+      rucEmisor: currentEmisor.rucEmisor
+    });
   }
 
   useEffect(() => {   
-    getDefault() 
+    getDataList(); 
+    getDefault(); 
   }, [])  
   
   return (
@@ -73,7 +85,22 @@ const CpeResumeHeaderSearch = () => {
                 // {...register('fechaHasta', { required: false })} 
               />
             </div>        
-
+            <div className='col'> 
+                <label className='d-flex align-items-center form-label mb-3'>
+                  Sede
+                </label>  
+                
+                <select
+                    name="Sucursal"
+                    className='form-control form-control-solid w-200px form-control-sm'
+                    {...register('Sucursal')}
+                  >
+                    <option value="-">Seleccione...</option> 
+                    {sede.map((elm, i) => (
+                      <option key={i} value={elm.sede}>{elm.sede}</option>
+                    ))}
+                  </select>
+              </div>    
             <div className='col'>  
               <label className='d-flex align-items-center form-label mb-3 invisible'>
                 .
