765a6ef28015dc52e24245c58b621529718dffb7
rafael pajuelo <rafaelpajuelot@gmail.com>
2025-09-02 11:12:19 -0500

    chore(UI): change new logo

--- src/apps/BackConfig.js
+++ src/apps/BackConfig.js
@@ -3,19 +3,19 @@ export const URL_BUCKET = 'https://jadalfecpe.s3.amazonaws.com/';
 
 export const FetchGet = async (endpoint) => {
     const response = await fetch(URL_BACK + endpoint);
-    const content = await response.json(); 
-    return content; 
+    const content = await response.json();
+    return content;
 }
 
 export const FetchConf = async (endpoint, method, data) => {
-  
+
     const config = {
         method: method,
         body: JSON.stringify(data),
         headers: { 'Content-type': 'application/json' }
     }
-    const response = await fetch(URL_BACK + endpoint, config); 
-    const content = await response.json(); 
+    const response = await fetch(URL_BACK + endpoint, config);
+    const content = await response.json();
     return content;
 }
 
--- src/apps/modules/auth/components/Login.js
+++ src/apps/modules/auth/components/Login.js
@@ -1,11 +1,11 @@
-import {useContext} from 'react'
-import {useState} from 'react'
+import { useContext } from 'react'
+import { useState } from 'react'
 import * as Yup from 'yup'
-import clsx from 'clsx' 
-import {useFormik} from 'formik' 
-import {useAuth} from '../core/Auth' 
-import {login, getEmisores} from '../services/AuthService';   
-import {Link} from 'react-router-dom'
+import clsx from 'clsx'
+import { useFormik } from 'formik'
+import { useAuth } from '../core/Auth'
+import { login, getEmisores } from '../services/AuthService';
+import { Link } from 'react-router-dom'
 import './Login.css';
 
 const loginSchema = Yup.object().shape({
@@ -27,25 +27,25 @@ const initialValues = {
 
 export function Login() {
   const [loading, setLoading] = useState(false)
-  const { setCurrentUser, setCurrentEmisor, setEmisores} = useAuth() 
+  const { setCurrentUser, setCurrentEmisor, setEmisores } = useAuth()
 
   const formik = useFormik({
     initialValues,
     validationSchema: loginSchema,
-    onSubmit: async (values, {setStatus, setSubmitting}) => {
+    onSubmit: async (values, { setStatus, setSubmitting }) => {
       setLoading(true)
-      try { 
+      try {
         const user = {
-          "emailUsuario":`${values.email}`,
-          "claveUsuario":`${values.password}`
-        } 
-        const data = await login(user) 
-        if (data.status == true) { 
+          "emailUsuario": `${values.email}`,
+          "claveUsuario": `${values.password}`
+        }
+        const data = await login(user)
+        if (data.status == true) {
           const user = {
-            "emailUsuario":`${values.email}`
-          } 
-          const emisores = await getEmisores(user)   
-          if (emisores) { 
+            "emailUsuario": `${values.email}`
+          }
+          const emisores = await getEmisores(user)
+          if (emisores) {
             // saveAuth(data) 
             setCurrentUser(data)
             // console.log('setCurrentUser', data);
@@ -54,21 +54,21 @@ export function Login() {
             setEmisores(emisores.content)
             localStorage.setItem('emisores.content', JSON.stringify(emisores.content));
 
-            setCurrentEmisor(emisores.content[0]);  
-            localStorage.setItem('emisores.content[0]', JSON.stringify(emisores.content[0])); 
-          }else{
+            setCurrentEmisor(emisores.content[0]);
+            localStorage.setItem('emisores.content[0]', JSON.stringify(emisores.content[0]));
+          } else {
             // saveAuth(undefined)
             setStatus('Usuario Incorrecto')
             setSubmitting(false)
             setLoading(false)
           }
-        }else{
+        } else {
           // saveAuth(undefined)
           setStatus('Usuario Incorrecto')
           setSubmitting(false)
           setLoading(false)
         }
- 
+
       } catch (error) {
         console.error(error)
         // saveAuth(undefined)
@@ -82,13 +82,13 @@ export function Login() {
   return (
     <form
       className='form w-100'
-        onSubmit={formik.handleSubmit}
+      onSubmit={formik.handleSubmit}
       noValidate
       id='kt_login_signin_form'
     >
       {/* begin::Heading */}
       <div className='text-center mb-11'>
-        <h1 className='text-dark fw-bolder mb-3'>Ingrese sus crendenciales 2</h1> 
+        <h1 className='text-dark fw-bolder mb-3'>Ingrese sus crendenciales</h1>
       </div>
       {/* begin::Heading */}
 
@@ -96,17 +96,17 @@ export function Login() {
         <div className='mb-lg-15 alert alert-danger'>
           <div className='alert-text font-weight-bold'>{formik.status}</div>
         </div>
-      ) }
-  
+      )}
+
       {/* begin::Form group */}
       <div className='fv-row mb-8'>
         <label className='form-label fs-6 fw-bolder text-dark'>Correo</label>
         <input
-          placeholder='Email' 
+          placeholder='Email'
           {...formik.getFieldProps('email')}
           className={clsx(
             'form-control bg-transparent',
-            {'is-invalid': formik.touched.email && formik.errors.email},
+            { 'is-invalid': formik.touched.email && formik.errors.email },
             {
               'is-valid': formik.touched.email && !formik.errors.email,
             }
@@ -156,7 +156,7 @@ export function Login() {
 
         {/* begin::Link */}
         <Link to='/auth/forgot-password' className='link-primary'>
-        ¿Has olvidado la contraseña?
+          ¿Has olvidado la contraseña?
         </Link>
         {/* end::Link */}
       </div>
@@ -172,7 +172,7 @@ export function Login() {
         >
           {!loading && <span className='indicator-label'>Ingresar</span>}
           {loading && (
-            <span className='indicator-progress' style={{display: 'block'}}>
+            <span className='indicator-progress' style={{ display: 'block' }}>
               Espere por favor...
               <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
             </span>
